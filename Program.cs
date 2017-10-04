e199e5f5481101fd9e029b6d954d7c73d4a79809
Jani Tarvainen <janit@iki.fi>
2017-10-04 23:20:38 +0300

    Initial import

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace yello
+{
+
+    class Animal {
+        public string type;
+        public string name;
+
+        public void setType(string newType){
+            type = newType;
+        }
+
+        public void setName(string newName){
+            name = newName;
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var animal = new Animal();
+            animal.setType("Pedobear");
+            animal.setName("Jorma");
+
+            var animalJson = JsonConvert.SerializeObject(animal);
+
+            Console.WriteLine(animalJson);
+        }
+    }
+}
